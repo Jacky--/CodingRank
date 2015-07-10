764f79288d4eee59f6731bdd2723ea98f2206df5
Jacky-- <wjz928@sina.cn>
2015-07-10 15:43:14 +0800

    Create CodingRank Project

---
+++ CodingRank/CodingRank/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CodingRank
+//
+//  Created by 吴家振 on 15/7/10.
+//  Copyright (c) 2015年 Jacky--. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CodingRank/CodingRank/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CodingRank
+//
+//  Created by 吴家振 on 15/7/10.
+//  Copyright (c) 2015年 Jacky--. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
